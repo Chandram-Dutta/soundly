977832a3a711c8ef26898a6c002fc464d220b4aa
Chandram-Dutta <chandramdutta2004@gmail.com>
2022-03-04 17:55:06 +0530

    Added Responsivness to the Widgets

--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -2,12 +2,50 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:soundly/page/home/widgets/theme_icon_button.dart';
+import 'package:soundly/responsive/responsive.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> _widgets = [
+      const Expanded(
+        flex: 2,
+        child: RiveAnimation.asset("assets/rive/soundly_rive.riv"),
+      ),
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.6,
+            height: 50,
+            child: ElevatedButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Icon(CupertinoIcons.play_arrow_solid),
+                  SizedBox(width: 10),
+                  Text("Start"),
+                ],
+              ),
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).colorScheme.secondary),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      )
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Soundly'),
@@ -15,41 +53,9 @@ class HomePage extends StatelessWidget {
           ThemeIconButton(),
         ],
       ),
-      body: Column(children: [
-        const Expanded(
-          flex: 2,
-          child: RiveAnimation.asset("assets/rive/soundly_rive.riv"),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.6,
-              child: ElevatedButton(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Icon(CupertinoIcons.play_arrow_solid),
-                    SizedBox(width: 10),
-                    Text("Start"),
-                  ],
-                ),
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                  ),
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      Theme.of(context).colorScheme.secondary),
-                ),
-                onPressed: () {},
-              ),
-            ),
-          ),
-        )
-      ]),
+      body: isDesktop(context, 800)
+          ? Row(children: _widgets)
+          : Column(children: _widgets),
     );
   }
 }
